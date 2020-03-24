82cc5a5fcf22b32864f2e3048580583b0578a718
Ershaad Basheer <ershaad.a@gmail.com>
2020-03-24 08:43:14 +0000

    try out without __KERNEL__ define

--- bpf/bpf_program.c
+++ bpf/bpf_program.c
@@ -1,10 +1,14 @@
+#include <linux/atomic.h>
 #include <linux/bpf.h>
 #include <bpf_helpers.h>
+#include <linux/ptrace.h>
 
-SEC("tracepoint/syscalls/sys_enter_execve")
-int bpf_prog(void *ctx) {
-  char msg[] = "Hello, BPF World!";
-  bpf_trace_printk(msg, sizeof(msg));
+SEC("kprobe/sys_write")
+int bpf_prog(struct pt_regs *ctx) {
+  long fd = PT_REGS_PARM1(ctx);
+  long count = PT_REGS_PARM3(ctx);
+  char msg[] = "fd=%d count=%d\n";
+  bpf_trace_printk(msg, sizeof(msg), fd, count);
   return 0;
 }
 
